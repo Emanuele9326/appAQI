8b441ceb8d1c59f405fcce8f74673ab7814cf90b
EMANUELE <65012625+Emanuele9326@users.noreply.github.com>
2021-05-27 21:28:08 +0200

    1.2

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin=require('html-webpack-plugin');
 const MiniCssExtractPlugin=require('mini-css-extract-plugin');
 const {CleanWebpackPlugin}=require('clean-webpack-plugin');
 module.exports={
-  mode:'development',
+  mode:'production',
 
   stats:{
     children:true,
